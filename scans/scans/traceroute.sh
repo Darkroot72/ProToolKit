2fa7c97c03ee91e4885ea99a4ad623a56e2e2e14
Darkroot72 <ramsesabundiz72@gmail.com>
2025-12-03 13:29:57 -0800

    Delete scans/scans directory

--- scans/scans/traceroute.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-
-traceroute_moderno() {
-log_header "Traceroute"
-if check_cmd mtr; then sudo mtr --report --report-cycles=3 "$HOST" 2>&1 | tee -a "$LOG_FILE" || true
-elif check_cmd traceroute; then traceroute -n "$HOST" 2>&1 | tee -a "$LOG_FILE" || true
-else log "${YELLOW}mtr/traceroute missing${RESET}"; fi
-}
